491772009f2f15fcd0f19c07009001c11b6562b7
Kaleb Muse <kcmuse@Kalebs-MacBook-Air.local>
2021-07-26 14:32:52 -0700

    added prompt

--- db/schema.sql
+++ db/schema.sql
@@ -4,26 +4,23 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY
     name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2),
     department_id INT NOT NULL,
-    PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES role(id)
 );
 
