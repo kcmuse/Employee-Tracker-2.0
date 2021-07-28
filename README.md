d3dcb68e3dbe0dd6e5844f3f1366c09817ca8def
Kaleb Muse <kcmuse@Kalebs-MacBook-Air.local>
2021-07-27 17:49:19 -0700

    fixed/cleaned code

---
+++ README.md
@@ -0,0 +1,47 @@
+# Employee Tracker 2.0
+
+## Description
+
+Users are given a database of employees. Within there they can create their own employees, edit current employees and view data on all employees. When started users are prompted with options and a series of questions.
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Questions](#questions)
+
+## Installation
+
+Below is the instructions to install the dependencies:
+
+```
+npm i
+```
+
+## Usage
+Here is a link to a video of how my application works.
+[Video](https://youtu.be/lieOGUreI_o)
+
+The following will start the program.
+```
+npm start
+```
+Users must run the following commands within mysql to seed/create tables properly
+```
+mysql -u (your username) -p(your password);
+source schema.sql;
+source seeds.sql;
+```
+
+## Contribution
+
+Please contact me via github/eMail
+
+
+## Questions
+
+For all questions please reach out to me at my email or github!
+### [GitHub](https://github.com/kcmuse)
+### [eMail](mailto:kcmusedev@gmail.com)
+
--- server.js
+++ server.js
@@ -11,49 +11,54 @@ const db = mysql.createConnection({
     console.log(`Connected to the employees database.`)
 );
 
-db.connect(function(err) {
+db.connect((err) => {
         if (err) throw err;
         console.log(`You have been connected!`);
         runProgram();
-
     })
     // TODO: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 
-function runProgram() {
+const runProgram = () => {
     inquirer.prompt({
-            name: ' business',
+            name: 'business',
             type: 'list',
             message: 'Please choose something to do!',
             choices: [
                 'View all employees',
                 'Add an employee',
-                'Update employee role',
+                // 'Update employee role',
                 'View all roles',
                 'Add a new role',
-                'View all deparments',
+                'View all departments',
                 'Add department'
             ]
         })
-        .then(function(answer) {
+        .then((answer) => {
             switch (answer.business) {
                 case 'View all employees':
                     viewAllEmployees();
                     break;
+
                 case 'Add an employee':
                     addEmployee();
                     break;
+
                 case 'Update employee role':
                     updateEmployeeRole();
                     break;
+
                 case 'View all roles':
                     viewAllRoles();
                     break;
-                case 'Add a role':
+
+                case 'Add a new role':
                     addRole();
                     break;
+
                 case 'View all departments':
-                    viewDeparments();
+                    viewDepartments();
                     break;
+
                 case 'Add department':
                     addDepartment();
                     break;
@@ -61,7 +66,7 @@ function runProgram() {
         });
 };
 // used to view all departments.
-function viewDeparments() {
+function viewDepartments() {
     console.log('Showing all departments\n');
     db.query(`SELECT * FROM department`, (err, res) => {
         if (err) throw err;
@@ -72,7 +77,7 @@ function viewDeparments() {
 // used to see all roles
 function viewAllRoles() {
     console.log(`Showing all roles\n`);
-    db.query(`SELECT * FROM role`, (err, res) => {
+    db.query(`SELECT id, title, salary FROM role`, (err, res) => {
         if (err) throw err;
         console.table(res);
         runProgram();
@@ -80,8 +85,18 @@ function viewAllRoles() {
 }
 // used to see all employees
 function viewAllEmployees() {
+    let allEmployee = `SELECT 
+    employee.id, 
+    first_name AS 'First name', 
+    last_name AS 'Last name', 
+    role.title AS 'Role', 
+    role.salary AS 'Salary', 
+    department.name AS 'Department' 
+    FROM employee
+    JOIN role ON role.id = employee.role_id
+    JOIN department ON department.id = role.department_id;`;
     console.log(`Showing all Employees\n`);
-    db.query(`SELECT * FROM employee`, (err, res) => {
+    db.query(allEmployee, (err, res) => {
         if (err) throw err;
         console.table(res);
         runProgram();
@@ -175,31 +190,32 @@ function addEmployee() {
     })
 
 }
+
 // Function to update the employee role
-function updateEmployeeRole() {
-    db.query(`SELECT CONCAT(first_name, " ", last_name) AS full_name FROM employee`, (emperr, empres) => {
-        if (emperr) throw emperr;
-        db.query(`SELECT title FROM role`, (rolerr, rolres) => {
-            if (rolerr) throw rolerr;
-            inquirer.prompt([{
-                    name: 'employee_id',
-                    type: 'list',
-                    message: 'Which employee would you edit?',
-                    choices: employeeRes.map(employee => employee.full_name)
-                },
-                {
-                    name: `newRole`,
-                    type: `list`,
-                    message: `Please choose a new role...`,
-                    choice: rolres.map(role => role.title)
-                }
-            ]).then((answer) => {
-                db.query(`UPDATE employee SET role_id = ? WHERE id = ?`, [answer.newRole, answer.employee_id], function(err) {
-                    if (err) throw err;
-                    console.log("Employee Role has been updated");
-                    runProgram();
-                })
-            })
-        })
-    })
-};
+// function updateEmployeeRole() {
+//     db.query(`SELECT first_name AS role_id FROM employee`, (emperr, empres) => {
+//         if (emperr) throw emperr;
+//         db.query(`SELECT title AS id FROM role`, (roleErr, roleRes) => {
+//             if (roleErr) throw roleErr;
+//             inquirer.prompt([{
+//                     name: 'employee_id',
+//                     type: 'list',
+//                     message: 'Which employee would you like to edit?',
+//                     choices: empres.map(employee => employee.role_id)
+//                 },
+//                 {
+//                     name: `newRole`,
+//                     type: `list`,
+//                     message: `Please choose a new role...`,
+//                     choices: roleRes.map(role => role.id)
+//                 }
+//             ]).then((answer) => {
+//                 db.query(`UPDATE employee SET role_id = ? WHERE id = ?`, [answer.newRole, answer.employee_id], (err) => {
+//                     if (err) throw err;
+//                     console.log("Employee Role has been updated");
+//                     runProgram();
+//                 })
+//             })
+//         })
+//     })
+// };
