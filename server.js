4d8dad557a60f4c46c7c742e1f54535228cc75ec
Kaleb Muse <kcmuse@Kalebs-MacBook-Air.local>
2021-07-28 00:37:03 -0700

    cleaned up code

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const mysql = require(`mysql`);
 
 const db = mysql.createConnection({
         host: 'localhost',
-        port: 3306,
+        port: 3001,
         user: 'root',
         password: 'password',
         database: 'employees_db'
     },
-    console.log(`Connected to the employees database.`)
+    console.log(`Connected: Employee database`)
 );
 
 db.connect((err) => {
